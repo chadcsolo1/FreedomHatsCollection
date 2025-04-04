ee7852116d38167f8b9f75afe619c0f9b94fa1a6
chad solomon <chadcsoloman@gmail.com>
2025-04-04 17:22:05 -0400

    Created Product Model, Product page.

---
+++ FreedomHatsCollection/Models/Product.cs
@@ -0,0 +1,35 @@
+﻿namespace FreedomHatsCollection.Models
+{
+    public class Product
+    {
+        public int Id
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        } = string.Empty;
+
+        public string ProductDescription
+        {
+            get;
+            set;
+        } = string.Empty;
+
+        public double Price
+        {
+            get;
+            set;
+        }
+
+        public bool IsActive
+        {
+            get;
+            set;
+        }   
+    }
+}
