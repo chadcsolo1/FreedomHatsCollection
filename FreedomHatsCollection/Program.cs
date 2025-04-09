de68318c77fc4d5f19aa88ecf033c92a13d96b4d
chad solomon <chadcsoloman@gmail.com>
2025-04-09 13:21:04 -0400

    CRUD for Orders and Products. Fixed Subtotals for Order creation

--- FreedomHatsCollection/Program.cs
+++ FreedomHatsCollection/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
-//builder.Services.AddBlazorBootstrap();
+builder.Services.AddBlazorBootstrap();
 builder.Services.AddDbContextFactory<ApplicationDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultSQLConnection")));
 
